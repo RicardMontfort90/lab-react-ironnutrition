ca590ed6dfba8af5a12deac224dc72cba851c08e
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-07 12:36:31 +0100

    WORK IN PROGRESS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import { useState } from "react";
-import { Card, Row, Col, Diver, Input, Button, Form } from "antd";
+import { Card, Row, Col, Divider, Input, Button, Form } from "antd";
 import foods from "./foods.json";
 import './App.css';
 
 function App() {
-  return <div className="App"></div>
+
+  const [foodList, setFoodList] = useState(foods);
+  const [searchText, setSearchText] = useState("");
+
+  const handleAddFood = (values) => {
+    const newFood = {
+      name: values.name,
+      image: values.image,
+      calories: values.calories,
+      servings: values.servings,
+    };
+    setFoodList([...foodList, newFood]);
+  };
+
+  return (
+    <div className="container">
+      <h1>IronNutrition</h1>
+      <Row>
+        <Col>
+        <Divider>Fancy Input</Divider>
+          <Input value={""} onChange={() => {}} />
+        </Col>
+      
+        <Col>
+          <Card title={"Fancy Card"}>
+            <Button onClick={() => {}}>Fancy Button</Button>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default App;
